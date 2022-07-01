5fda524eca39c4fb560760fd691c8eae15bd31ee
arun73822 <arun73822@gmail.com>
2022-07-01 15:39:42 +0530

    changes

--- housing/logger/__init__.py
+++ housing/logger/__init__.py
@@ -3,7 +3,7 @@ import os
 from datetime import datetime
 
 LOG_DIR= 'housing_logs'
-CURRENT_TIME_STAMP= f"{datetime.now().strftime('%d-%m-%Y__%I:%M:%S')}"
+CURRENT_TIME_STAMP= f"{datetime.now().strftime('%d-%m-%Y_%I:%M:%S_%p')}"
 LOG_FILE_NAME= f"log_{CURRENT_TIME_STAMP}.log"
 
 os.makedirs(LOG_DIR,exist_ok=True)
