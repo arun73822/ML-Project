dfd6b9431a4258273dd749ffc40402737c2ec160
arun73822 <arun73822@gmail.com>
2022-07-02 15:31:40 +0530

    modified

--- housing/constants/__init__.py
+++ housing/constants/__init__.py
@@ -6,7 +6,7 @@ CONFIG_DIR= "config"
 CONFIG_FILE_NAME= "config.yaml"
 CONFIG_FILE_PATH= os.path.join(ROOT_DIR,CONFIG_DIR,CONFIG_FILE_NAME)
 
-CURRENT_TIME_STAMP = f"{datetime.now().strftime('%Y-%m-%d-%I-%M-%S')}"
+CURRENT_TIME_STAMP = f"{datetime.now().strftime('%Y-%m-%d-%I-%M-%S_%p')}"
 
 # Training_Pipeline_related_variables
 TRAINING_PIPELINE_CONFIG_KEY= "training_pipeline_config"
